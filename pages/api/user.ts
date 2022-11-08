086e2fcde23dce2363c9b1c238479a7e62f054a1
Ivan Lara <ukikalix@gmail.com>
2022-11-07 18:26:12 -0600

    fx: set default thumbnail if api thumbnail is empty wheter user is a new one or not

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,38 +1,35 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getUser, createUser } from "@services/userService";
-import { getFollowersByUser } from "@services/followedService";
-import { UserProps } from "types/data/user";
-import { UserQueryType } from "types/services/user";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getUser, createUser } from '@services/userService'
+import { getFollowersByUser } from '@services/followedService'
+import { UserProps } from 'types/data/user'
+import { UserQueryType } from 'types/services/user'
 
 export default async function user(
-    req: NextApiRequest,
-    res: NextApiResponse<{ user: UserProps }>
+  req: NextApiRequest,
+  res: NextApiResponse<{ user: UserProps }>
 ) {
-    try {
-        const payload = JSON.parse(req.body);
+  try {
+    const payload = JSON.parse(req.body)
 
-        let user = await getUser(
-            String(payload?.nickname),
-            UserQueryType.ONE_USER
-        );
+    let user = await getUser(String(payload?.nickname), UserQueryType.ONE_USER)
 
-        if (!user.thumbnail) {
-            user.thumbnail = {
-                basename: user.username,
-                url: `https://api.multiavatar.com/${user.username}.png`,
-            };
-        }
-
-        if (user) {
-            const followers = await getFollowersByUser(user.id);
-            user.followers = followers;
-        } else {
-            user = (await createUser(payload)) as UserProps;
-        }
-
-        res.status(200).json({ user: { ...user, issuer: payload?.nickname } });
-    } catch (error: any) {
-        console.log(error);
-        res.status(500).end(error.message);
+    if (user) {
+      const followers = await getFollowersByUser(user.id)
+      user.followers = followers
+    } else {
+      user = (await createUser(payload)) as UserProps
     }
+
+    if (!user.thumbnail) {
+      user.thumbnail = {
+        basename: user.username,
+        url: `https://api.multiavatar.com/${user.username}.png`,
+      }
+    }
+
+    res.status(200).json({ user: { ...user, issuer: payload?.nickname } })
+  } catch (error: any) {
+    console.log(error)
+    res.status(500).end(error.message)
+  }
 }
