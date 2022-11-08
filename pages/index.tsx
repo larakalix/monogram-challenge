b63ca853dabfab0d10fd8667ec2a81159fd79dc8
Ivan Lara <ukikalix@gmail.com>
2022-11-08 01:01:40 -0600

    fx: clean index login form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,29 @@
+import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0'
 import { Loader } from '@components/generic'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { ImTwitter as ImMweeter } from 'react-icons/im'
-import { FormField } from 'types/data/formField'
-
-type InputProps = {
-  email: string
-}
-
-const initialValues: InputProps = {
-  email: '',
-}
-
-const formFields: FormField[] = [{ label: 'Email address', name: 'email' }]
 
 export default function Index() {
-  const { user, error, isLoading } = useUser()
-  const { push } = useRouter()
+    const { user } = useUser()
 
-  if (user) return <Loader />
+    if (user) return <Loader />
 
-  return (
-    <section className="flex items-center justify-center flex-col h-screen">
-      <ImMweeter className="w-12 h-12 text-main-blue" />
-      <h1 className="text-heading-gray font-extrabold text-[1.5rem] leading-[1.65rem] mt-2">
-        mweeter
-      </h1>
+    return (
+        <section className="flex items-center justify-center flex-col h-screen">
+            <ImMweeter className="w-12 h-12 text-main-blue" />
+            <h1 className="text-heading-gray font-extrabold text-[1.5rem] leading-[1.65rem] mt-2">
+                mweeter
+            </h1>
 
-      <div className="mt-4 p-2 text-center max-w-sm w-full">
-        <Link
-          href="/api/auth/login"
-          passHref
-          className="bg-primary-button text-white rounded-md py-[0.813rem] px-[1.969rem] mt-4 w-full"
-        >
-          Login
-        </Link>
-      </div>
-    </section>
-  )
+            <div className="mt-4 p-2 text-center max-w-sm w-full">
+                <Link
+                    href="/api/auth/login"
+                    passHref
+                    className="bg-primary-button text-white rounded-md py-[0.813rem] px-[1.969rem] mt-4 w-full"
+                >
+                    Login
+                </Link>
+            </div>
+        </section>
+    )
 }
